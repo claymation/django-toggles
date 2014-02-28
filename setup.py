ec0fd7b6773d4baaebdc1be481e8f5371fb45195
Clay McClure <clay@daemons.net>
2014-02-27 22:01:07 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-toggles',
+    version='0.1',
+    packages=['toggles'],
+    include_package_data=True,
+    license='MIT License',
+    description='',
+    long_description=README,
+    url='https://github.com/claymation/django-toggles',
+    author='Clay McClure',
+    author_email='clay@daemons.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
